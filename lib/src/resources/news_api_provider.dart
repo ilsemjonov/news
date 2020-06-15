85a569bf89131bcb21f93e530773f4920cc98a33
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-15 20:20:24 +0300

    fetchTopIds

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,10 +1,14 @@
 import 'package:http/http.dart' show Client;
+import 'dart:convert';
 
 class NewsApiProvider {
   Client client = Client();
 
-  fetchTopIds(){
-    client.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  fetchTopIds() async {
+    final response = await client.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    final ids = json.decode(response.body);
+
+    return ids;
   }
 
   fetchItem(){}
