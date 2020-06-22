eedc5094c5e930bcced2c0c7c3cb85f0dd298925
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-22 21:54:04 +0300

    buildList() preview method

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -6,7 +6,33 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('will show news here'),
+      body: Text('Show a list'),
     );
   }
+
+  // Widget buildList() {
+  //   return ListView.builder(
+  //     itemCount: 1000,
+  //     itemBuilder: (context, int index) {
+  //       return FutureBuilder(
+  //         future: getFuture(),
+  //         builder: (context, snapshot) {
+  //           return Container(
+  //             height: 80.0,
+  //             child: snapshot.hasData
+  //                 ? Text('visible $index')
+  //                 : Text('not visible yet $index'),
+  //           );
+  //         },
+  //       );
+  //     },
+  //   );
+  // }
+
+  // getFuture() {
+  //   return Future.delayed(
+  //     Duration(seconds: 2),
+  //     () => 'hi',
+  //   );
+  // }
 }
