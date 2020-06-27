ff18ddcc7f858788588ee272e1093c3ea9ee8752
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-27 19:01:57 +0300

    stories_bloc.dart

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,1 +1,14 @@
-class StoriesBloc {}
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class StoriesBloc {
+  final _topIds = PublishSubject<List<int>>();
+
+  // getter to stream
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  dispose() {
+    _topIds.close();
+  }
+}
