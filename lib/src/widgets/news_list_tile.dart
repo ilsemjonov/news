69c9d4a9c2e57a94a0f284a9855c335375878e30
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-28 15:47:49 +0300

    news_list_tile.dart init

---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  NewsListTile({this.itemId});
+
+  Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+  }
+}
