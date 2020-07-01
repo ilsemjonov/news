a6527021779ce507cdce1e1f3d24ca246680b0c6
Ilja Semjonov <ilsemjonov@gmail.com>
2020-07-01 14:25:27 +0300

    displaying story details -> title

--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/models/item_model.dart';
+import '../blocs/comments_provider.dart';
 
 class NewsDetail extends StatelessWidget {
   final int itemId;
@@ -6,11 +8,51 @@ class NewsDetail extends StatelessWidget {
   NewsDetail({this.itemId});
 
   Widget build(context) {
+    final bloc = CommentsProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Detail'),
       ),
-      body: Text('$itemId'),
+      body: buildBody(bloc),
+    );
+  }
+
+  Widget buildBody(CommentsBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.itemWithComments,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('loading');
+        }
+
+        final itemFuture = snapshot.data[itemId];
+
+        return FutureBuilder(
+          future: itemFuture,
+          builder: (context, AsyncSnapshot<ItemModel> itemSnapshot) {
+            if (!itemSnapshot.hasData) {
+              return Text('loading');
+            }
+
+            return buildTitle(itemSnapshot.data);
+          },
+        );
+      },
+    );
+  }
+
+  Widget buildTitle(ItemModel item) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      child: Text(
+        item.title,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
