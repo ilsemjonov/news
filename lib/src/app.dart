384b1b11a53a6e3c07d7faae1070acb69f0a49a3
Ilja Semjonov <ilsemjonov@gmail.com>
2020-07-01 11:25:47 +0300

    connecting commentsProvider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'screens/news_list.dart';
 import 'blocs/stories_provider.dart';
 import 'screens/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   Widget build(context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        title: 'News',
-        // home: NewsList(),
-        onGenerateRoute: routes,
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          title: 'News',
+          // home: NewsList(),
+          onGenerateRoute: routes,
+        ),
       ),
     );
   }
@@ -24,7 +27,11 @@ class App extends StatelessWidget {
     } else {
       return MaterialPageRoute(
         builder: (context) {
+          final commentsBloc = CommentsProvider.of(context);
           final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+          commentsBloc.fetchItemWithComments(itemId);
+
           return NewsDetail(
             itemId: itemId,
           );
